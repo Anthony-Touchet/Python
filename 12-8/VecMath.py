81e28838822ff2a9fdd53699c6e5e3a4e6798f5b
atouchetAIE <tocheck3@yahoo.com>
2015-12-08 17:47:52 -0600

    Vector Library in Python

--- 12-8/VecMath.py
+++ 12-8/VecMath.py
@@ -6,27 +6,68 @@ class Vector(object):
 		self.y = ny
 		self.z = nz
 		
-	def stats(self):
+	def stats(self): #Print
 		print(self.x,",",self.y,",",self.z)
 		
-	def __add__(self, other):
+	def __add__(self, other): #Addition
 		result = Vector(0,0,0)
 		result.x = self.x + other.x
 		result.y = self.y + other.y
 		result.z = self.z + other.z
 		return result
 		
-	def __sub__(self, other):
+	def __sub__(self, other): #Subtraction
 		result = Vector(0,0,0)
 		result.x = self.x - other.x
 		result.y = self.y - other.y
 		result.z = self.z - other.z
 		return result
 		
-	def Magnitude(self):
+	def Magnitude(self): #Magnitude
 		result = Vector(0,0,0)
 		result.x = self.x ^ 2
 		result.y = self.y ^ 2
 		result.z = self.z ^ 2
 		newre = sqrt(result.x + result.y + result.z)
 		return newre
+		
+	def Normalize(self): #Normalize
+		result = Vector(0,0,0)
+		result.x = self.x / self.z
+		result.y = self.y / self.z
+		result.z = self.z / self.z
+		return result
+		
+	def __mul__(self, other): #Dot Product
+		x = self.x + other.x
+		y = self.y + other.y
+		z = self.z + other.z
+		return x + y + z
+		
+	def Cross(self, other): #Cross Product
+		result = Vector(0,0,0)
+		result.x = (self.y * other.z) - (other.y + self.z)
+		result.y = (self.z * other.x) - (other.z + self.x)
+		result.z = (self.x * other.y) - (other.x + self.y)
+		return result
+		
+	def lerp(self, other, per):
+		result = Vector(0,0,0)
+		result.x = self.x + (per * (other.x - self.x))
+		result.y = self.y + (per * (other.y - self.y))
+		result.z = self.z + (per * (other.z - self.z))
+		return result
+		
+	
+def DtoR(object):
+		return (object / 180) * 3.14
+	
+def RtoD(object):
+	return (object / 3.14) * 180		
+		
+def HexColor(object):
+	red = int(object[:2], 16)
+	green = int(object[2:4], 16)
+	blue = int(object[4:6], 16)
+	alpha = int(object[6:], 16)
+	print(red,green,blue,alpha)
--- 12-8/test.py
+++ 12-8/test.py
@@ -1,16 +1,53 @@
 import VecMath
 from VecMath import *
 
-a = Vector(1,2,3)
-b = Vector(3,2,1)
+a = Vector(1,2,60)
+b = Vector(5,2,96)
 c = Vector(0,0,0)
+result = Vector(0,0,0)
+print(" ")
 
 c = a + b
 print("Addition: ")
 c.stats()
 print(" ")
 
-print("Magnitu".Magnitude())
+print("Magnitue:",c.Magnitude(),"\n")
 
+print("Subtraction: ")
 c = a - b
 c.stats()
+print(" ")
+
+c = a + b
+print("Normalize:")
+result = c.Normalize()
+result.stats()
+print(" ")
+
+print("Dot Product:")
+print(a * b)
+print(" ")
+
+print("Cross Product:")
+result = a.Cross(b)
+result.stats()
+print(" ")
+
+print("LinearInterpolation:")
+result = a.lerp(b,.5)
+result.stats()
+print(" ")
+
+degrees = 180
+print("Degrees to Radians:")
+print(DtoR(degrees))
+degrees = DtoR(degrees)
+print(" ")
+
+print("Radians to Degrees:")
+print(RtoD(degrees))
+print(" ")
+
+print("HexColor:")
+HexColor("DA3778D5")
